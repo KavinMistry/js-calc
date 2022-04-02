2ebac31552241dba7eecb532e01e17df05fc596f
Kavin Mistry <73570657+KavinMistry@users.noreply.github.com>
2022-04-02 09:36:02 +0530

    Made code shorter

---
+++ copy.js
@@ -0,0 +1,12 @@
+var a = +prompt(' Enter the value of the first term: ');
+var b = +prompt(' Enter the value of the second term: ');
+
+function CalczS() {
+    var c = a + b
+    alert(" " + c )
+    }
+
+function CalczM() {
+    var c = a * b
+    alert(" " + c )
+    }
